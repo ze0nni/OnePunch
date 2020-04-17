92268bf822dc77a6af5470cd1263edc069737638
e.blagodarev <ze0nni@gmail.com>
2020-04-18 00:17:12 +0300

    Load game assets from json

---
+++ Assets/Code/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public PlayerPanelHierarchy left;
+    public PlayerPanelHierarchy right;
+
+    private Data gameData;
+
+    void Start()
+    {
+        this.gameData = JsonUtility.FromJson<Data>(Resources.Load<TextAsset>("data").text);
+    }
+}
