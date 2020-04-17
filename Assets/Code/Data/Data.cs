473bfb9089d59e85f25a049e64aa8d1312551f8d
e.blagodarev <ze0nni@gmail.com>
2020-04-17 14:01:54 +0300

    Import 'TestAssigmentPackage'

---
+++ Assets/Code/Data/Data.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+[Serializable]
+public class Data
+{
+    public GameModel settings;
+    public Stat[] stats;
+    public Buff[] buffs;
+}
+
+[Serializable]
+public class GameModel
+{
+    public int buffCountMin;
+    public int buffCountMax;
+    public bool allowDuplicateBuffs;
+
+}
+
+[Serializable]
+public class Stat
+{
+    public int id;
+    public string title;
+    public string icon;
+    public float value;
+}
+
+[Serializable]
+public class BuffStat
+{
+    public float value;
+    public int statId;
+}
+
+[Serializable]
+public class Buff
+{
+    public string icon;
+    public int id;
+    public string title;
+    public BuffStat[] stats;
+}
