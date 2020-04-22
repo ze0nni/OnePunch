6d4c77bfe84ade92e04d92a05502ef77dcce6655
e.blagodarev <ze0nni@gmail.com>
2020-04-23 00:26:00 +0300

    Add 'AbsorbDamageByStatValueTests'

---
+++ Assets/Code/Battle/Aspects/AbsorbDamageByStatValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle.Aspects
+{
+    public class AbsorbDamageByStatValue : BattleAspect
+    {
+        readonly private int statId;
+
+        public AbsorbDamageByStatValue(int statId)
+        {
+            this.statId = statId;
+        }
+
+        public OnBeforeHitResult OnBeforeHit(Fighter source, Fighter consumer, float baseDamage, OnBeforeHitResult result)
+        {
+            result.currentDamage = (result.currentDamage * (100 - consumer.Stat(statId))) / 100f;
+            return result;
+        }
+    }
+}
---
+++ Assets/Editor/Aspects/AbsorbDamageByStatValueTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle.Aspects
+{
+    class AbsorbDamageByStatValueTests
+    {
+
+        [Test]
+        public void AbsorbDamageByStatValue_no_effect() {
+            var aspect = new AbsorbDamageByStatValue(0);
+
+            var consumer = new TestFighter();
+            consumer.getStat = (id) => 0;
+
+            var result = aspect.OnBeforeHit(null, consumer, 100, new OnBeforeHitResult(100, false));
+
+            Assert.AreEqual(
+                new OnBeforeHitResult(100, false),
+                result
+            );
+        }
+
+        [Test]
+        public void AbsorbDamageByStatValue_absorb_30_percents_of_damage()
+        {
+            var aspect = new AbsorbDamageByStatValue(0);
+
+            var consumer = new TestFighter();
+            consumer.getStat = (id) => 30;
+
+            var result = aspect.OnBeforeHit(null, consumer, 0, new OnBeforeHitResult(100, false));
+
+            Assert.AreEqual(
+                new OnBeforeHitResult(70, false),
+                result
+            );
+        }
+    }
+}
