a30911d95e821f4e5cb151357296962b8e6ce33c
e.blagodarev <ze0nni@gmail.com>
2020-04-23 12:37:27 +0300

    Add 'StatsFromConfigTests'

---
+++ Assets/Code/Battle/Stats.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+    public interface Stats
+    {
+        float Get(int statId);
+    }
+
+    public sealed class StatsFromConfig : Stats
+    {
+        readonly Dictionary<int, Stat> stats;
+        readonly Buff[] buffs;
+
+        public StatsFromConfig(Stat[] stats, Buff[] buffs)
+        {
+            this.stats = stats.ToDictionary(s => s.id, s => s);
+            this.buffs = buffs;
+        }
+
+        public float Get(int statId)
+        {
+            return BuffsExtensions.Apply(
+                buffs,
+                statId,
+                this.stats[statId].value
+            );
+        }
+    }
+}
---
+++ Assets/Editor/StatsFromConfigTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+    class StatsFromConfigTests
+    {
+        [Test]
+        public void Test_values_froms_stats()
+        {
+            var stats = new StatsFromConfig(
+                new Stat[] {
+                        new Stat() { id = 0, value = 10 },
+                        new Stat() { id = 1, value = 20 },
+                        new Stat() { id = 2, value = 30 },
+                        new Stat() { id = 3, value = 40 },
+                },
+                new Buff[] { }
+            );
+
+            Assert.AreEqual(10, stats.Get(0));
+            Assert.AreEqual(20, stats.Get(1));
+            Assert.AreEqual(30, stats.Get(2));
+            Assert.AreEqual(40, stats.Get(3));
+        }
+
+        [Test]
+        public void Test_values_froms_stats_and_bufss()
+        {
+            var stats = new StatsFromConfig(
+                new Stat[] {
+                    new Stat() { id = 0, value = 20 },
+                    new Stat() { id = 1, value = 10 },
+                    new Stat() { id = 2, value = 10 },
+                    new Stat() { id = 3, value = 10 },
+                },
+                new Buff[] {
+                    new Buff() {
+                        stats = new BuffStat[] {
+                            new BuffStat() { statId = 0, value = -10 }
+                        }
+                    },
+                    new Buff() {
+                        stats = new BuffStat[] {
+                            new BuffStat() { statId = 1, value = 10 },
+                            new BuffStat() { statId = 2, value = 20 }
+                        }
+                    },
+                    new Buff() {
+                        stats = new BuffStat[] {
+                            new BuffStat() { statId = 3, value = 30 }
+                        }
+                    }
+                }
+            );
+
+            Assert.AreEqual(10, stats.Get(0));
+            Assert.AreEqual(20, stats.Get(1));
+            Assert.AreEqual(30, stats.Get(2));
+            Assert.AreEqual(40, stats.Get(3));
+        }
+    }
+}
