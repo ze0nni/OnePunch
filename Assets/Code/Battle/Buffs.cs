12a3c8d4acf759df01f23c8db3a6b66576ebe86f
e.blagodarev <ze0nni@gmail.com>
2020-04-23 16:07:47 +0300

    Add 'NoDuplcatesBuffsFactory' and 'DuplcatesBuffsFactory'

---
+++ Assets/Code/Battle/Buffs.cs
@@ -0,0 +1,52 @@
+﻿using Assets.Code.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+
+    public interface BuffsFactory
+    {
+        Buff[] Produce(int amount);
+    }
+
+    // Не стал делать тесты для Random но технически можно и это проверить
+
+    public sealed class NoDuplcatesBuffsFactory : BuffsFactory
+    {
+        readonly Buff[] buffs;
+
+        public NoDuplcatesBuffsFactory(Buff[] buffs)
+        {
+            this.buffs = buffs;
+        }
+
+        public Buff[] Produce(int amount)
+        {
+            return new Shuffled<Buff>(buffs).Take(amount).ToArray();
+        }
+    }
+
+    public sealed class DuplcatesBuffsFactory: BuffsFactory {
+        readonly Buff[] buffs;
+
+        public DuplcatesBuffsFactory(Buff[] buffs)
+        {
+            this.buffs = buffs;
+        }
+
+        public Buff[] Produce(int amount)
+        {
+            var outputSize = Math.Min(amount, buffs.Length);
+            var output = new Buff[outputSize];
+            for (var i = 0; i < outputSize; i++) {
+                output[i] = buffs[UnityEngine.Random.Range(0, buffs.Length)];
+            }
+            return output;
+        }
+    }
+
+}
---
+++ Assets/Code/Random/Shuffled.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Random
+{
+    public class Shuffled<T> : IEnumerable<T>
+    {
+        readonly IEnumerable<T> origin;
+
+        public Shuffled(IEnumerable<T> origin)
+        {
+            this.origin = origin;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var source = origin.ToArray();
+            var size = source.Length;
+            if (0== size) {
+                yield break;
+            }
+
+            for (var i = 0; i < size - 1; i++) {
+                var n = UnityEngine.Random.Range(i, size);
+                if (n != i) {
+                    var tmp = source[n];
+                    source[n] = source[i];
+                    source[i] = tmp;
+                }
+                yield return source[i];
+            }
+
+            yield return source[size - 1];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
